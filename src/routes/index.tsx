ac4039946420e069182ba21225e4b13bd5969d83
jerrynim <tjerry3@naver.com>
2023-01-29 16:08:03 +0900

    delete main

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,7 @@
 import { component$, useStyles$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
-import cityPlan from "@qwik-city-plan";
 import styles from "./index.css?inline";
 
 export default component$(() => {
     useStyles$(styles);
-    console.log(cityPlan.routes);
-    return (
-        <div>
-            <nav>
-                {cityPlan.routes.map((route) => {
-                    return (
-                        route[3].includes("posts") && (
-                            <Link
-                                class="route-path"
-                                href={route[3]}
-                                key={route[3]}
-                            >
-                                {route[3]}
-                            </Link>
-                        )
-                    );
-                })}
-            </nav>
-        </div>
-    );
+    return <div>only for organic search</div>;
 });
