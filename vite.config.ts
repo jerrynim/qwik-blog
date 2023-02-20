1e201e56f8e50c623f44e707e0abe4e9192ef803
jerrynim <tjerry3@naver.com>
2023-02-20 23:50:33 +0900

    simtemap post제거

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig(() => {
                     //? index.ts파일 경로
                     urls.push(_path.replace(`${__dirname}/src/routes`, ""));
                 } else if (
+                    filename.includes("[") ||
                     filename.endsWith(".css") ||
                     filename === "service-worker.ts" ||
                     filename === "menu.md" ||
@@ -38,21 +39,12 @@ export default defineConfig(() => {
             .join("");
 
         const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <sitemap>
-        <loc>https://jerrynim.dev/sitemap_post.xml</loc>
-      </sitemap>
-    </sitemapindex>`;
-        const sitemapPost = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${urlTags}
     </urlset>`;
         try {
             fs.writeFileSync(`${__dirname}/public/sitemap.xml`, sitemap);
-            fs.writeFileSync(
-                `${__dirname}/public/sitemap_post.xml`,
-                sitemapPost,
-            );
+            fs.writeFileSync(`${__dirname}/public/sitemap.xml`, sitemap);
         } catch (e) {
             console.warn(e);
         }
