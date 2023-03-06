df1742902cc1bcd373dba6d5d2395c1973003a1b
jerrynim <tjerry3@naver.com>
2023-03-06 23:39:25 +0900

    update main

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,14 @@
 import { component$, useStyles$ } from "@builder.io/qwik";
+import PageHeader from "../components/page-header";
 import styles from "./index.css?inline";
 
 export default component$(() => {
     useStyles$(styles);
-    return <div>only for organic search</div>;
+    return (
+        <div>
+            <PageHeader />
+            <h1>제리님 블로그</h1>
+            only for organic search
+        </div>
+    );
 });
