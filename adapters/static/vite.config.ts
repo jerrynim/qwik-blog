b4139a8ad8731214e78241ac02ec0e2e7639f782
tjerry3@naver.com <tjerry3@naver.com>
2023-06-16 19:33:29 +0900

    생성은 되데 크롤링만 안되게

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -13,7 +13,6 @@ export default extendConfig(baseConfig, () => {
         plugins: [
             staticAdapter({
                 origin: "https://jerrynim.dev",
-                exclude: ["/portfolio/*", "/posts/*"],
             }),
         ],
     };
