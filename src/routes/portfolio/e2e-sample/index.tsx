459f7c1ec8c2e28ed3897f81d5956fd25ca7f819
jerrynim <tjerry3@naver.com>
2023-11-18 18:50:32 +0900

    menu 및 portfolio 업데이트

--- src/routes/portfolio/e2e-sample/index.tsx
+++ src/routes/portfolio/e2e-sample/index.tsx
@@ -9,10 +9,10 @@ export default component$(() => {
         <>
             <PageHeader></PageHeader>
             <div class="contents">
-                <p class="title">Zuix2 E2E 구성도</p>
-                <PostImage src="https://res.cloudinary.com/dij9kacx9/image/upload/v1674781890/lit-blog/zuix-e2e_gdawtr.png"></PostImage>
                 <p class="title">Zuix2 E2E 결과 페이지</p>
-                <PostImage src="https://res.cloudinary.com/dij9kacx9/image/upload/v1674782128/lit-blog/zuix2-e2e-result_egmupe.png"></PostImage>
+                <PostImage src="https://res.cloudinary.com/dij9kacx9/image/upload/v1700300897/e2e-sample_baufc0.png"></PostImage>
+                <PostImage src="https://res.cloudinary.com/dij9kacx9/image/upload/v1700300898/e2e-onion_cz1sn9.png"></PostImage>
+                <PostImage src="https://res.cloudinary.com/dij9kacx9/image/upload/v1700300899/e2e-swiper_rqqkzp.png"></PostImage>
                 <p class="title">Zuix1 E2E 결과</p>
                 <PostImage src="https://res.cloudinary.com/dij9kacx9/image/upload/v1674785815/lit-blog/zuix1-e2e_hblxun.png"></PostImage>
             </div>
