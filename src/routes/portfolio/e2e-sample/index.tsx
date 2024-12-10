5b95de457d86c02a663d29be36c0026517691985
jerrynim <jerrynim@doodlin.co.ko>
2024-12-10 22:34:13 +0900

    e2e-sample alt 추가

--- src/routes/portfolio/e2e-sample/index.tsx
+++ src/routes/portfolio/e2e-sample/index.tsx
@@ -9,12 +9,24 @@ export default component$(() => {
         <>
             <PageHeader></PageHeader>
             <div class="contents">
-                <p class="title">Zuix2 E2E 결과 페이지</p>
-                <PostImage src="https://res.cloudinary.com/dij9kacx9/image/upload/v1700300897/e2e-sample_baufc0.png"></PostImage>
-                <PostImage src="https://res.cloudinary.com/dij9kacx9/image/upload/v1700300898/e2e-onion_cz1sn9.png"></PostImage>
-                <PostImage src="https://res.cloudinary.com/dij9kacx9/image/upload/v1700300899/e2e-swiper_rqqkzp.png"></PostImage>
+                <p class="title">Zuix2 스냅샷 테스트 결과 페이지</p>
+                <PostImage
+                    src="https://res.cloudinary.com/dij9kacx9/image/upload/v1700300897/e2e-sample_baufc0.png"
+                    alt="변경된 UI 검출 가능"
+                ></PostImage>
+                <PostImage
+                    src="https://res.cloudinary.com/dij9kacx9/image/upload/v1700300898/e2e-onion_cz1sn9.png"
+                    alt="두 이미지를 겹쳐서 확인 가능"
+                ></PostImage>
+                <PostImage
+                    src="https://res.cloudinary.com/dij9kacx9/image/upload/v1700300899/e2e-swiper_rqqkzp.png"
+                    alt="두 이미지를 Swiper로 확인 가능"
+                ></PostImage>
                 <p class="title">Zuix1 E2E 결과</p>
-                <PostImage src="https://res.cloudinary.com/dij9kacx9/image/upload/v1674785815/lit-blog/zuix1-e2e_hblxun.png"></PostImage>
+                <PostImage
+                    src="https://res.cloudinary.com/dij9kacx9/image/upload/v1674785815/lit-blog/zuix1-e2e_hblxun.png"
+                    alt="Zuix1 의 UI 변경 검출"
+                ></PostImage>
             </div>
         </>
     );
