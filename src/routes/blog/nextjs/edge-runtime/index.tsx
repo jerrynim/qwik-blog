be930ec8aa1472ba4d4b8e6fbde5d3e39e413276
jerrynim <jerrynim@doodlin.co.ko>
2025-12-07 17:35:44 +0900

    [master] 문법수정

--- src/routes/blog/nextjs/edge-runtime/index.tsx
+++ src/routes/blog/nextjs/edge-runtime/index.tsx
@@ -12,15 +12,15 @@ import {
 } from "@components/post";
 
 export const head: DocumentHead = {
-  title: "proxy 사용시 edge runtime을 사용하는 이유와 제약",
+  title: "Next.js에서 Edge Runtime을 사용하는 이유와 제약",
   meta: [
     {
       property: "keywords",
-      content: "nextjs edge runtime proxy",
+      content: "nextjs edge runtime proxy v8 isolate",
     },
     {
       property: "description",
-      content: "proxy 사용시 edge runtime을 사용하는 이유와 제약",
+      content: "Next.js에서 Edge Runtime을 사용하는 이유와 제약",
     },
   ],
 };
@@ -30,7 +30,7 @@ export default component$(() => {
     <>
       <PostHeadImage src=""></PostHeadImage>
       <PostHead>
-        <h1>proxy 사용시 edge runtime을 사용하는 이유와 제약</h1>
+        <h1>Next.js에서 Edge Runtime을 사용하는 이유와 제약</h1>
         <PostTag tags="nextjs edge runtime proxy"></PostTag>
         <PostDate>2025-12-07</PostDate>
       </PostHead>
@@ -48,10 +48,10 @@ export default component$(() => {
         </PostLink>{" "}
         사용시 제한된 API를 제공하는 런타임입니다.
         <br />
-        Edge runtime을 사용함으로서 다음과 같음 이점을 가질수 있습니다.
+        Edge runtime을 사용함으로써 다음과 같은 이점을 가질 수 있습니다.
         <ul>
           <li>Web Standard API 기반의 경량화된 런타임</li>
-          <li>빠른 cord start(Edge worker의 V8 isolate 사용시)</li>
+          <li>빠른 cold start(Edge worker의 V8 isolate 사용 시)</li>
           <li>Edge 환경의 지리적으로 가까운 위치에서 응답을 제공</li>
         </ul>
         Vercel/Cloudflare로 배포하게된다면 Proxy는 전세계 edge locations에{" "}
@@ -62,7 +62,7 @@ export default component$(() => {
         <br />
         만약 self-hosting 환경이라면 어떻게 Proxy가 동작할까요?
         <br />
-        Porxy는 Node.js의 vm 모듈을 사용하여 격리된 JavaScript 실행 환경을
+        Proxy는 Node.js의 vm 모듈을 사용하여 격리된 JavaScript 실행 환경을
         만듭니다.
         <PostCode
           code={code}
@@ -70,13 +70,13 @@ export default component$(() => {
           filename="packages/next/src/server/web/sandbox/context.ts"
         />
         <br />
-        따라서 단일 Node.js 프로세스환경에서 Node.js 힙을 공유하게되며 Edge
+        따라서 단일 Node.js 프로세스 환경에서 Node.js 힙을 공유하게 되며 Edge
         Runtime의 이점을 가지지 못합니다.
         <br />
-        하지만 Next.js 는 동일한 환경을 제공하기위해 제한된 Node.js API제공등의
-        작업을 수행합니다.
+        하지만 Next.js는 동일한 환경을 제공하기 위해 제한된 Node.js API 제공
+        등의 작업을 수행합니다.
         <br />
-        process.env 를 사용할 수 있게 합니다.
+        process.env를 사용할 수 있게 합니다.
         <PostCode code={code2} language="typescript" />
         <PostLink href="https://nextjs.org/docs/app/api-reference/edge#reference">
           제한된 API set
@@ -86,8 +86,8 @@ export default component$(() => {
         Web Standard API를 Next.js 요구사항에 맞게 커스터마이징합니다.
         <PostCode code={code4} language="typescript" />
         <br />
-        ISR을 사용할 수 없는이유도 동일합니다. ISR은 파일 시스템 캐싱을
-        사용하여야하지만 Edge Runtime에서는 fs 모듈 사용이 불가합니다.
+        ISR을 사용할 수 없는 이유도 동일합니다. ISR은 파일 시스템 캐싱을
+        사용해야 하지만 Edge Runtime에서는 fs 모듈 사용이 불가합니다.
         <br />
         <br />
         참조 링크 :
